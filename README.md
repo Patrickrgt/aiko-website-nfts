d18ad08be6c658b86374ed712fa75b37830a4e0c
Chase Manning <me@chasemanning.co.nz>
2022-02-17 18:51:56 +0000

    :tada: setup boilerplate

---
+++ README.md
@@ -0,0 +1,3 @@
+# Aiko Website
+
+TODO
---
+++ src/app/globals.ts
@@ -0,0 +1,13 @@
+export const INFURA_ID = "ce27c65027c64f7a8ea17c6e63dfae96";
+
+interface GlobalsType {
+  WETH: string;
+}
+
+export const ETH_GLOBALS: GlobalsType = {
+  WETH: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+};
+
+export const RINKEBY_GLOBALS: GlobalsType = {
+  WETH: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+};
