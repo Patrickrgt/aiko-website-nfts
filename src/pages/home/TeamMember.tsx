a4c6ea6fb00bf8491a036510406e799d316e6313
Chase Manning <me@chasemanning.co.nz>
2022-02-23 19:49:27 +0000

    :rocket: image and border to team members

--- src/pages/home/TeamMember.tsx
+++ src/pages/home/TeamMember.tsx
@@ -7,10 +7,55 @@ export interface TeamMemberType {
 }
 
 const StyledTeamMember = styled.div`
+  position: relative;
   display: flex;
-  background: pink;
   width: 100%;
   aspect-ratio: 1;
+  padding: 5px;
+  background: #fcd04d;
+  clip-path: polygon(
+    30% 0%,
+    70% 0%,
+    100% 30%,
+    100% 70%,
+    70% 100%,
+    30% 100%,
+    0% 70%,
+    0% 30%
+  );
+`;
+
+const InnerBorder = styled.div`
+  position: relative;
+  width: 100%;
+  aspect-ratio: 1;
+  background: #8493b9;
+  padding: 2px;
+  clip-path: polygon(
+    30% 0%,
+    70% 0%,
+    100% 30%,
+    100% 70%,
+    70% 100%,
+    30% 100%,
+    0% 70%,
+    0% 30%
+  );
+`;
+
+const Image = styled.img`
+  width: 100%;
+  aspect-ratio: 1;
+  clip-path: polygon(
+    30% 0%,
+    70% 0%,
+    100% 30%,
+    100% 70%,
+    70% 100%,
+    30% 100%,
+    0% 70%,
+    0% 30%
+  );
 `;
 
 interface Props {
@@ -19,7 +64,16 @@ interface Props {
 }
 
 const TeamMember = ({ index, teamMember }: Props) => {
-  return <StyledTeamMember>meow</StyledTeamMember>;
+  return (
+    <StyledTeamMember>
+      <InnerBorder>
+        <Image
+          src={teamMember.image}
+          alt={`${teamMember.name} profile picture`}
+        />
+      </InnerBorder>
+    </StyledTeamMember>
+  );
 };
 
 export default TeamMember;
