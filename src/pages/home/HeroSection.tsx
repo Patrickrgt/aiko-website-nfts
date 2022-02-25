c56b67312a8c16a579f87cdb215a3c532eda81b4
Chase Manning <me@chasemanning.co.nz>
2022-02-25 23:05:30 +0000

    :rocket: add content to hero section

--- src/pages/home/HeroSection.tsx
+++ src/pages/home/HeroSection.tsx
@@ -1,21 +1,80 @@
 import styled from "styled-components";
-import Logo from "../../components/Logo";
+
 import NavItems from "../../components/NavItems";
 import Section from "../../components/Section";
 
-const LogoContainer = styled.div`
+import heroImage from "../../assets/illustrations/hero.png";
+import decal from "../../assets/svgs/hero-decal.svg";
+
+const HeroContainer = styled.div`
+  position: relative;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+`;
+
+const OrangeBlock = styled.div`
   position: absolute;
-  top: 8rem;
-  left: 50%;
-  transform: translateX(-50%);
+  left: 0;
+  bottom: 17%;
+  width: 100vw;
+  height: 59%;
+  background: #ffcf5f;
+  overflow: hidden;
+`;
+
+const OrangeDecal = styled.img`
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  height: 73%;
+`;
+
+const LeftDecal = styled(OrangeDecal)`
+  left: -3px;
+`;
+
+const RightDecal = styled(OrangeDecal)`
+  right: -3px;
+  transform: translateY(-50%) rotate(180deg);
+`;
+
+const ImageContainer = styled.div`
+  position: relative;
+`;
+
+const HeroImage = styled.img`
+  height: 78vh;
+  transform: translateX(1.8%);
+`;
+
+const InfoText = styled.div`
+  position: absolute;
+  bottom: 10.4%;
+  left: 38.5%;
+  height: 6.6%;
+  width: 40%;
+  display: flex;
+  align-items: center;
+  color: #6b9bd0;
+  font-size: 2.5rem;
+  font-weight: 400;
+  letter-spacing: 5px;
 `;
 
 const HeroSection = () => {
   return (
     <Section socials index={1} bottomPlus>
-      <LogoContainer>
-        <Logo />
-      </LogoContainer>
+      <HeroContainer>
+        <OrangeBlock>
+          <LeftDecal src={decal} alt="Decorative element" />
+          <RightDecal src={decal} alt="Decorative element" />
+        </OrangeBlock>
+        <ImageContainer>
+          <HeroImage src={heroImage} alt="Hero image" />
+          <InfoText>(A:\MintDate\TBA)</InfoText>
+        </ImageContainer>
+      </HeroContainer>
       <NavItems />
     </Section>
   );
