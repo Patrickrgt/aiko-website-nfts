bd248f8c991d75603470f63ef8a76a8e4769ce60
Chase Manning <me@chasemanning.co.nz>
2022-02-26 15:55:41 +0000

    :rocket: add content to gallery section

--- src/pages/home/GallerySection.tsx
+++ src/pages/home/GallerySection.tsx
@@ -2,6 +2,9 @@ import styled from "styled-components";
 import Section from "../../components/Section";
 import background from "../../assets/svgs/gallery-bg.svg";
 import Header from "../../components/Header";
+import AikoHex from "../../components/AikoHex";
+
+import chase from "../../assets/team-members/chase.jpg";
 
 const Container = styled.div`
   position: relative;
@@ -14,18 +17,24 @@ const Background = styled.img`
 `;
 
 const HeaderContainer = styled.div`
-  margin-top: 2rem;
-`;
-
-const Content = styled.div`
   position: absolute;
   top: 0;
   left: 0;
   width: 100%;
-  height: 100%;
   display: flex;
-  flex-direction: column;
-  align-items: center;
+  justify-content: center;
+  margin-top: 2.7rem;
+`;
+
+const Content = styled.div`
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  left: 0;
+  width: 100%;
+  height: calc(100% - 18rem);
+  display: flex;
+  background: pink;
 `;
 
 const GallerySection = () => {
@@ -33,10 +42,11 @@ const GallerySection = () => {
     <Section topPlus bottomPlus>
       <Container>
         <Background src={background} alt="Gallery background" />
+        <HeaderContainer>
+          <Header>Be Part of our Universe</Header>
+        </HeaderContainer>
         <Content>
-          <HeaderContainer>
-            <Header>Be Part of our Universe</Header>
-          </HeaderContainer>
+          <AikoHex image={chase} mega />
         </Content>
       </Container>
     </Section>
