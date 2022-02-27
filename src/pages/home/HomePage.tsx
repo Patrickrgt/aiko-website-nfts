f7fda1f356e306d82180df48be26836942f55138
Chase Manning <me@chasemanning.co.nz>
2022-02-27 20:07:25 +0000

    💩 add todo items

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -5,6 +5,23 @@ import RoadmapSection from "./RoadmapSection";
 import StorySection from "./StorySection";
 import TeamSection from "./TeamSection";
 
+// Update prixie face
+// Sticky socials
+// Fade in elements
+// Update preview images
+// Text update :Charity: A:\Charity -> Holders will decide between 3 charities to donate every month.
+// :Airdrop:  A:\Airdrop  ->  airdrop exclusive genesis Aiko NFTs to holders, in addition to 1/1 merch such as high quality t-shirts and hoodies (see Season 2)
+// Just change our roadmap name to Virtualmap on the site
+// Horazontal scroll
+// Gap between team members
+// Text not aligned on hero and team numbers
+// Implement navigation
+// Wide screen support
+// Small screen support
+// Story logo add white background
+// Add team member info
+// Add secret team member
+
 const StyledHomePage = styled.div`
   width: 100%;
   display: flex;
