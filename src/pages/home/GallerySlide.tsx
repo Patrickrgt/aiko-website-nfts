240aeab41a483ce30067b018004764144a7fefb6
Chase Manning <me@chasemanning.co.nz>
2022-02-27 20:16:03 +0000

    :lipstick: update gallery preview images

--- src/pages/home/GallerySlide.tsx
+++ src/pages/home/GallerySlide.tsx
@@ -3,6 +3,15 @@ import styled from "styled-components";
 
 import AikoHex from "../../components/AikoHex";
 
+import a1 from "../../assets/gallery/1.jpg";
+import a2 from "../../assets/gallery/2.jpg";
+import a3 from "../../assets/gallery/3.jpg";
+import a4 from "../../assets/gallery/4.jpg";
+import a5 from "../../assets/gallery/5.jpg";
+import a6 from "../../assets/gallery/6.jpg";
+import a7 from "../../assets/gallery/7.jpg";
+import a8 from "../../assets/gallery/8.jpg";
+import a9 from "../../assets/gallery/9.jpg";
 import chase from "../../assets/team-members/chase.jpg";
 import crystal from "../../assets/team-members/crystal.jpg";
 import garrid from "../../assets/team-members/garrid.jpg";
@@ -12,24 +21,22 @@ import matarelli from "../../assets/team-members/matarelli.jpg";
 import vinne from "../../assets/team-members/vinne.jpg";
 
 const aikos: string[] = [
+  a1,
+  a2,
+  a3,
+  a4,
+  a5,
+  a6,
+  a7,
+  a8,
+  a9,
   vinne,
-  crystal,
-  garrid,
-  kinoko,
   kyo,
-  matarelli,
-  chase,
-  vinne,
-  crystal,
   garrid,
-  kinoko,
-  kyo,
   matarelli,
-  chase,
-  vinne,
   crystal,
-  garrid,
   kinoko,
+  chase,
 ];
 
 const StyledGallerySlide = styled.div`
@@ -98,7 +105,7 @@ interface Props {
 }
 
 const GallerySlide = ({ setMainAiko }: Props) => {
-  const [active, setActive] = useState(6);
+  const [active, setActive] = useState(3);
 
   return (
     <StyledGallerySlide>
