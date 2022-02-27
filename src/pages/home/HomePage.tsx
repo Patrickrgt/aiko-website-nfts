112b51b7054d3e6bef5769c58b4b99ddc4a38a8a
Chase Manning <me@chasemanning.co.nz>
2022-02-27 20:47:48 +0000

    :sparkles: add loggin

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -5,10 +5,8 @@ import RoadmapSection from "./RoadmapSection";
 import StorySection from "./StorySection";
 import TeamSection from "./TeamSection";
 
-// Update prixie face
 // Sticky socials
 // Fade in elements
-// Update preview images
 // Text update :Charity: A:\Charity -> Holders will decide between 3 charities to donate every month.
 // :Airdrop:  A:\Airdrop  ->  airdrop exclusive genesis Aiko NFTs to holders, in addition to 1/1 merch such as high quality t-shirts and hoodies (see Season 2)
 // Just change our roadmap name to Virtualmap on the site
