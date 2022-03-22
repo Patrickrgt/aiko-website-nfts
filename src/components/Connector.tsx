269139e7c362a200fb6d5786c33a22cbd7056751
Chase Manning <me@chasemanning.co.nz>
2022-03-22 07:43:46 +0000

    :lipstick: style connector

--- src/components/Connector.tsx
+++ src/components/Connector.tsx
@@ -1,16 +1,31 @@
+import styled from "styled-components";
 import { shortenAddress, useEthers } from "@usedapp/core";
-import { useDispatch } from "react-redux";
-import { connectWallet } from "../state/uiSlice";
+// import { useDispatch } from "react-redux";
+// import { connectWallet } from "../state/uiSlice";
 import Button from "./Button";
 
+const StyledConnector = styled.div`
+  position: absolute;
+
+  top: 7rem;
+  right: 7rem;
+  @media only screen and (max-width: 1400px) {
+    top: 3rem;
+    right: 3rem;
+  }
+`;
+
 const Connector = () => {
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch();
   const { account } = useEthers();
 
   return (
-    <Button click={() => dispatch(connectWallet())}>
-      {account ? shortenAddress(account) : "Connect Wallet"}
-    </Button>
+    <StyledConnector>
+      {/* <Button click={() => dispatch(connectWallet())}> */}
+      <Button primary click={() => console.log("meow")}>
+        {account ? shortenAddress(account) : "<A:\\Connect Wallet>"}
+      </Button>
+    </StyledConnector>
   );
 };
 
