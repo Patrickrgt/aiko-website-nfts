21e6dd5815ea16bb0abc4b23419fcb7150ae111f
Chase Manning <me@chasemanning.co.nz>
2022-03-23 12:45:12 +0000

    :rocket: add content to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,30 @@
 import styled from "styled-components";
 
+import certified from "../assets/svgs/certified.svg";
+import etherscan from "../assets/svgs/etherscan.svg";
+import meepo from "../assets/svgs/meepo.svg";
+import mirror from "../assets/svgs/mirror.svg";
+
+interface LinkType {
+  icon: string;
+  url: string;
+}
+
+const links: LinkType[] = [
+  {
+    icon: mirror,
+    url: "",
+  },
+  {
+    icon: etherscan,
+    url: "",
+  },
+  {
+    icon: meepo,
+    url: "",
+  },
+];
+
 const StyledFooter = styled.div`
   position: relative;
   width: 100%;
@@ -58,9 +83,35 @@ const WhiteOverlay = styled.div`
   left: 0;
 `;
 
+const Certified = styled.img`
+  height: 100%;
+`;
+
+const Links = styled.div`
+  display: flex;
+  height: 100%;
+`;
+
+const Link = styled.a`
+  height: 100%;
+`;
+
+const Icon = styled.img`
+  height: 100%;
+  margin-left: 2.3rem;
+`;
+
 const Footer = () => {
   return (
     <StyledFooter>
+      <Certified src={certified} alt="Aiko Certified" />
+      <Links>
+        {links.map((link: LinkType) => (
+          <Link href={link.url} target="_blank" rel="noopener noreferrer">
+            <Icon src={link.icon} alt="Aiko Certified" />
+          </Link>
+        ))}
+      </Links>
       <DecalOuter>
         <DecalMiddle>
           <DecalInner />
