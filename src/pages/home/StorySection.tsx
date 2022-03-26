901719d098a522fb4d0d971668c80b2039d7090a
garridspen <90074461+garridspen@users.noreply.github.com>
2022-03-25 22:26:33 -0300

    Changed the Story Text -> By Garrid
    Changed the Story text
    
    Here is the old one: 
                  Aiko Virtual is all about "Cyber Cute", which combines future tech
                  with anime vibes to create a unique aesthetic with broad appeal
                  and an even broader vision. Vinne is the artistic mastermind
                  behind our visual direction while Kyo is our resident fashion and
                  design ronin. Our goal is simple: create a premium concept
                  straddling the line between art and fashion , the digital and
                  physical, and East and West. Aikos represents a never-before-seen
                  concept rich in lore and personalization. Join us on our journey
                  into the virtual unknown.

--- src/pages/home/StorySection.tsx
+++ src/pages/home/StorySection.tsx
@@ -116,15 +116,16 @@ const StorySection = () => {
           <Header>A:\About us</Header>
           <AikoFade>
             <Body>
-              Aiko Virtual is all about "Cyber Cute", which combines future tech
-              with anime vibes to create a unique aesthetic with broad appeal
-              and an even broader vision. Vinne is the artistic mastermind
-              behind our visual direction while Kyo is our resident fashion and
-              design ronin. Our goal is simple: create a premium concept
-              straddling the line between art and fashion , the digital and
-              physical, and East and West. Aikos represents a never-before-seen
-              concept rich in lore and personalization. Join us on our journey
-              into the virtual unknown.
+              Aiko Virtual is a 8,888 collection on Ethereum Blockchain about "Cyber Cute", 
+              a term used by them to designate the mix of future tech with anime vibes to
+              create a unique aesthetic with broad appeal and an even broader vision.
+             
+              Vinne is the artistic mastermind behind our visual direction while Kyo is our resident fashion and design
+              ronin. Our goal is simple: create a premium concept straddling the line between art
+              and fashion , the digital and physical, and East and West. 
+              
+              Aikos represents a never-before-seen concept rich in lore and personalization. 
+              Join us on our journey into the virtual unknown. Use "A:\" be virtual.
             </Body>
           </AikoFade>
           <div />
