95cca9106d6a4d024f7002a1947b89adcb7aa5fa
chase-manning <chase@manning.dev>
2022-05-01 14:57:09 +0100

    :sparkles: create tabs component

---
+++ src/components/Tabs.tsx
@@ -0,0 +1,82 @@
+import styled from "styled-components";
+
+interface TabType {
+  label: string;
+  content: JSX.Element;
+}
+
+const tabs: TabType[] = [
+  {
+    label: "archives",
+    content: <div>archives</div>,
+  },
+  {
+    label: "info",
+    content: <div>info</div>,
+  },
+  {
+    label: "history",
+    content: <div>history</div>,
+  },
+];
+
+const StyledTabs = styled.div`
+  display: flex;
+  position: absolute;
+  top: 15rem;
+  left: 20rem;
+`;
+
+interface TabProps {
+  color: string;
+}
+
+const Tab = styled.button`
+  position: relative;
+  cursor: pointer;
+  width: 16rem;
+  height: 3rem;
+  background: ${(props: TabProps) => props.color};
+  margin-right: -1.2rem;
+  clip-path: polygon(
+    13% 0%,
+    100% 0%,
+    100% 0%,
+    100% 100%,
+    100% 100%,
+    0% 100%,
+    0% 100%,
+    0% 70%
+  );
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #b0a17b;
+  font-size: 1.8rem;
+  font-weight: 700;
+`;
+
+const Dot = styled.div`
+  width: 6px;
+  height: 6px;
+  position: absolute;
+  top: 2px;
+  right: 3px;
+  background: #b0a17b;
+  border-radius: 50%;
+`;
+
+const Tabs = () => {
+  return (
+    <StyledTabs>
+      {tabs.map((tab, index) => (
+        <Tab key={tab.label} color={index % 2 === 0 ? "#FFDF6C" : "#FFED90"}>
+          {tab.label}
+          <Dot />
+        </Tab>
+      ))}
+    </StyledTabs>
+  );
+};
+
+export default Tabs;
