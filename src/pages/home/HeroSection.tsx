8fd8574bbc7a0b52882e27dacfae84a4ff7e192a
chase-manning <chase@manning.dev>
2022-05-09 08:50:02 +0100

    :lipstick: implement home redesign

--- src/pages/home/HeroSection.tsx
+++ src/pages/home/HeroSection.tsx
@@ -6,8 +6,6 @@ import Logo from "../../components/Logo";
 
 import heroImage from "../../assets/illustrations/hero.png";
 import heroImageMobile from "../../assets/illustrations/hero-mobile.png";
-import decal from "../../assets/svgs/hero-decal.svg";
-import AikoFade from "../../components/AikoFade";
 import Button from "../../components/Button";
 import Connector from "../../components/Connector";
 import { useDevice } from "../../app/hooks/use-device";
@@ -25,52 +23,13 @@ const HeroContainer = styled.div`
   }
 `;
 
-const LogoContainer = styled.div`
-  position: absolute;
-  left: 50%;
-  transform: translateX(-50%);
-  top: -2.2rem;
-
-  @media only screen and (max-width: 600px) {
-    top: -7.6rem;
-  }
-`;
-
-const OrangeBlock = styled.div`
-  position: absolute;
-  left: 0;
-  bottom: 17%;
-  width: 100vw;
-  height: 59%;
-  background: #ffcf5f;
-  overflow: hidden;
-`;
-
-const OrangeDecal = styled.img`
-  position: absolute;
-  top: 50%;
-  transform: translateY(-50%);
-  height: 73%;
-
-  @media only screen and (max-width: 600px) {
-    display: none;
-  }
-`;
-
-const LeftDecal = styled(OrangeDecal)`
-  left: -3px;
-`;
-
-const RightDecal = styled(OrangeDecal)`
-  right: -3px;
-  transform: translateY(-50%) rotate(180deg);
-`;
-
 const ImageContainer = styled.div`
   position: relative;
   display: flex;
   justify-content: center;
-  align-items: center;
+  align-items: flex-start;
+  height: 66vh;
+  width: 100%;
   @media only screen and (max-width: 600px) {
     transform: translateY(7.4%);
     right: 0;
@@ -79,39 +38,11 @@ const ImageContainer = styled.div`
 `;
 
 const HeroImage = styled.img`
-  transform: translateX(1.8%);
-  height: 78vh;
+  transform: translateX(5%);
+  height: 94%;
   @media only screen and (max-width: 600px) {
     transform: none;
-    width: calc(100% - 4rem);
     height: auto;
-  }
-`;
-
-const InfoText = styled.div`
-  position: absolute;
-  display: flex;
-  align-items: center;
-  color: #6b9bd0;
-
-  font-size: 2.6rem;
-  letter-spacing: 10px;
-  font-weight: 400;
-  bottom: 10.3%;
-  left: 38.5%;
-  height: 6.1%;
-  width: 40%;
-  @media only screen and (max-width: 1400px) {
-    font-size: 2.3rem;
-  }
-  @media only screen and (max-width: 600px) {
-    bottom: 10%;
-    left: 24.3%;
-    height: 5.6%;
-    width: 40%;
-    font-size: 1.2rem;
-    letter-spacing: 2px;
-    font-weight: 400;
   }
 `;
 
@@ -134,10 +65,10 @@ const ButtonContainer = styled.div`
   justify-content: center;
   align-items: center;
 
-  top: 38.6%;
-  left: 3.15%;
-  width: 13%;
-  height: 30%;
+  top: 45%;
+  left: 14%;
+  width: 10.8%;
+  height: 30.3%;
   @media only screen and (max-width: 600px) {
     display: none;
   }
@@ -149,31 +80,21 @@ const HeroSection = () => {
   return (
     <Section id="home-scroll">
       <HeroContainer>
-        <OrangeBlock>
-          <LeftDecal src={decal} alt="Decorative element" />
-          <RightDecal src={decal} alt="Decorative element" />
-        </OrangeBlock>
-        <AikoFade>
-          <ImageContainer>
-            <LogoContainer>
-              <Logo primary />
-            </LogoContainer>
-            <HeroImage
-              src={isMobile ? heroImageMobile : heroImage}
-              alt="Hero image"
-            />
-            <InfoText>A:\MintDate\TBA</InfoText>
-            <Video autoPlay muted loop>
-              <source src="/assets/flying.webm" type="video/webm" />
-            </Video>
-            <ButtonContainer>
-              <Button disabled click={() => console.log("meow")}>
-                {"<mint.exe>"}
-              </Button>
-            </ButtonContainer>
-            <Tabs />
-          </ImageContainer>
-        </AikoFade>
+        <ImageContainer>
+          <HeroImage
+            src={isMobile ? heroImageMobile : heroImage}
+            alt="Hero image"
+          />
+          <Video autoPlay muted loop>
+            <source src="/assets/flying.webm" type="video/webm" />
+          </Video>
+          <ButtonContainer>
+            <Button disabled click={() => console.log("meow")}>
+              {"<mint.exe>"}
+            </Button>
+          </ButtonContainer>
+          <Tabs />
+        </ImageContainer>
       </HeroContainer>
       <Connector />
       <NavItems />
