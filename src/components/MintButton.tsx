c63b82aa5d8b3540550301f6733d357c2b241c58
chase-manning <chase@manning.dev>
2022-06-11 21:37:53 +0100

    :wrench: update launch time

--- src/components/MintButton.tsx
+++ src/components/MintButton.tsx
@@ -88,7 +88,7 @@ const MintButton = () => {
   const disabled = true;
 
   const tick = useTick();
-  const END = new Date(1655586000000);
+  const END = new Date(1656172800000);
   const now = new Date();
   const remaining = END.getTime() - now.getTime();
 
