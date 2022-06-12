5b1d3f80fbc38cc165b9c5ab4c01ffc6cc24b093
chase-manning <chase@manning.dev>
2022-06-12 12:55:33 +0100

    :sparkles: change confirmation to use opensea button

--- src/pages/mint/MintConfirmation.tsx
+++ src/pages/mint/MintConfirmation.tsx
@@ -1,9 +1,8 @@
 import styled from "styled-components";
-import { OPENSEA } from "../../app/globals";
 
 import decal from "../../assets/mint/confirmation-header.svg";
 import illustration from "../../assets/mint/confirmation-illustration.svg";
-import opensea from "../../assets/mint/opensea.svg";
+import MintOpenseaButton from "./MintOpenseaButton";
 
 const TopContent = styled.div`
   position: relative;
@@ -37,52 +36,6 @@ const Image = styled.img`
   width: 36rem;
 `;
 
-const ButtonContainer = styled.a`
-  display: flex;
-  cursor: pointer;
-  background: #f7d173;
-  width: 35.5rem;
-  padding: 5px;
-  clip-path: polygon(
-    4.55% 0%,
-    95.45% 0%,
-    100% 25%,
-    100% 75%,
-    95.45% 100%,
-    4.55% 100%,
-    0% 75%,
-    0% 25%
-  );
-`;
-
-const Opensea = styled.img`
-  height: 5.2rem;
-  margin-right: 5px;
-`;
-
-const ButtonLabel = styled.div`
-  flex: 1;
-  height: 100%;
-  background: #56729e;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  color: white;
-  font-size: 2.1rem;
-  font-weight: 400;
-  white-space: nowrap;
-  clip-path: polygon(
-    5% 0%,
-    95% 0%,
-    100% 25%,
-    100% 75%,
-    95% 100%,
-    5% 100%,
-    0% 75%,
-    0% 25%
-  );
-`;
-
 const MintConfirmation = () => {
   return (
     <>
@@ -93,10 +46,7 @@ const MintConfirmation = () => {
         <SubHeader>welcome to aiko virtual</SubHeader>
       </TopContent>
       <Image src={illustration} alt="Illustration" />
-      <ButtonContainer href={OPENSEA} target="_blank" rel="noopener noreferrer">
-        <Opensea src={opensea} alt="Opensea Icon" />
-        <ButtonLabel>{"<View In Opensea.exe>"}</ButtonLabel>
-      </ButtonContainer>
+      <MintOpenseaButton />
     </>
   );
 };
