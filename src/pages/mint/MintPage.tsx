dc54a8c64b46b9edb9ab411b63450f9a2d448c31
chase-manning <chase@manning.dev>
2022-06-12 12:30:14 +0100

    :lipstick: update mint page overlay

--- src/pages/mint/MintPage.tsx
+++ src/pages/mint/MintPage.tsx
@@ -88,7 +88,8 @@ const Overlay = styled.div`
   left: 0;
   width: 100%;
   height: 100%;
-  background: rgba(0, 0, 0, 0.5);
+  background: rgba(255, 255, 255, 0.5);
+  backdrop-filter: blur(2px);
   z-index: 1;
 `;
 
