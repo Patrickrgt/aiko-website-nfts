466939211d701e4aab014c1be38eb91923d56c78
chase-manning <chase@manning.dev>
2022-06-13 19:03:36 +0100

    :rocket: add countdown functionality to mint stage

--- src/pages/mint/MintStage.tsx
+++ src/pages/mint/MintStage.tsx
@@ -2,6 +2,15 @@ import styled from "styled-components";
 
 import Hexify from "../../components/Hexify";
 import timer from "../../assets/mint/timer.svg";
+import {
+  useFirstSaleEndTime,
+  useFirstSaleStartTime,
+  useSecondSaleStartTime,
+  useSecondSaleEndTime,
+  useHolderSaleStartTime,
+  useHolderSaleEndTime,
+} from "../../contracts/views";
+import { useTick } from "../../app/hooks/use-tick";
 
 const Stage = styled.div`
   position: relative;
@@ -51,14 +60,52 @@ const Countdown = styled.div`
   margin-right: 4px;
 `;
 
+const padZeros = (num: number) => {
+  return num < 10 ? `0${num}` : num;
+};
+
 const MintStage = () => {
+  const tick = useTick();
+
+  const now = new Date().getTime() / 1000;
+
+  const fistSaleStartTime = useFirstSaleStartTime();
+  const firstSaleEndTime = useFirstSaleEndTime();
+  const secondSaleEndTime = useSecondSaleEndTime();
+  const holderSaleEndTime = useHolderSaleEndTime();
+
+  const stage = () => {
+    if (now < fistSaleStartTime) return "error1";
+    if (now <= firstSaleEndTime) return "one";
+    if (now <= secondSaleEndTime) return "two";
+    if (now <= holderSaleEndTime) return "three";
+    return "error2";
+  };
+
+  const stageEnd = () => {
+    if (now <= firstSaleEndTime) return new Date(firstSaleEndTime * 1000);
+    if (now <= secondSaleEndTime) return new Date(secondSaleEndTime * 1000);
+    if (now <= holderSaleEndTime) return new Date(holderSaleEndTime * 1000);
+    return new Date();
+  };
+
+  const remaining = stageEnd().getTime() - new Date().getTime();
+
+  const hours = Math.floor(
+    (remaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  const minutes = Math.floor((remaining % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((remaining % (1000 * 60)) / 1000);
+
   return (
     <Hexify dark>
       <Stage>
-        {"<stage one>"}
+        {`<stage ${stage()}>`}
         <CountdownContainer>
           <Image src={timer} alt="image" />
-          <Countdown>12:12:12</Countdown>
+          <Countdown>{`${padZeros(hours)}:${padZeros(minutes)}:${padZeros(
+            seconds
+          )}`}</Countdown>
         </CountdownContainer>
       </Stage>
     </Hexify>
