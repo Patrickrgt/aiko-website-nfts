38c0544813e61ec3259ea457f6bdd280ec84e938
chase-manning <chase@manning.dev>
2022-06-15 17:14:43 +0100

    :rocket: add content to mint pending

--- src/pages/mint/MintPending.tsx
+++ src/pages/mint/MintPending.tsx
@@ -1,6 +1,10 @@
 import styled from "styled-components";
 
 import bg from "../../assets/mint/pending.jpg";
+import decal from "../../assets/mint/pending-decal.svg";
+import timer from "../../assets/mint/pending-timer.svg";
+import { useIsPending, useNextStage } from "../../contracts/views";
+import { useTick } from "../../app/hooks/use-tick";
 
 const StyledMintPending = styled.div`
   position: absolute;
@@ -26,13 +30,92 @@ const Content = styled.div`
   position: relative;
   display: flex;
   flex-direction: column;
+  align-items: center;
+`;
+
+const Decal = styled.img`
+  width: 100%;
+  width: 61rem;
+`;
+
+const Text = styled.div`
+  font-weight: 700;
+  color: #43679a;
+  text-align: center;
+  text-transform: uppercase;
+  line-height: 0.8;
+`;
+
+const TopText = styled(Text)`
+  font-size: 7rem;
+`;
+
+const BottomText = styled(Text)`
+  font-size: 8.3rem;
+`;
+
+const Countdown = styled.div`
+  display: flex;
+  align-items: center;
+  width: 61rem;
+  height: 12rem;
+  background: #43679a;
+  padding: 1.5rem;
+  margin-top: 1rem;
+  clip-path: polygon(
+    5% 0%,
+    95% 0%,
+    100% 25%,
+    100% 75%,
+    95% 100%,
+    5% 100%,
+    0% 75%,
+    0% 25%
+  );
+`;
+
+const Icon = styled.img`
+  height: 100%;
+`;
+
+const Numbers = styled.div`
+  height: 100%;
+  flex: 1;
+  font-size: 10rem;
+  color: white;
+  font-weight: 900;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 const MintPending = () => {
+  const tick = useTick();
+  const time = useNextStage();
+  const show = useIsPending();
+
+  if (!show) return null;
+
+  const remaining = time.getTime() - new Date().getTime();
+
+  const hours = Math.floor(
+    (remaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  const minutes = Math.floor((remaining % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((remaining % (1000 * 60)) / 1000);
+
   return (
     <StyledMintPending>
       <Background src={bg} alt="background" />
-      <Content>meow</Content>
+      <Content>
+        <Decal src={decal} alt="decal" />
+        <TopText>next mint stage</TopText>
+        <BottomText>will begin in</BottomText>
+        <Countdown>
+          <Icon src={timer} alt="timer icon" />
+          <Numbers>{`${hours}:${minutes}:${seconds}`}</Numbers>
+        </Countdown>
+      </Content>
     </StyledMintPending>
   );
 };
