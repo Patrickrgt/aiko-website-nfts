febfb33d08f4507af433aca0e307880cf27c0a5a
chase-manning <chase@manning.dev>
2022-06-16 11:00:07 +0100

    :sparkles: add mint functions

--- src/contracts/functions.ts
+++ src/contracts/functions.ts
@@ -3,14 +3,38 @@ import useContract from "../app/hooks/use-contract";
 
 import abi from "./aiko.json";
 
-export const useWrap = () => {
+export const useMintFirstOrb = () => {
   const globals = useGlobals();
 
-  const { state: wrapState, send: wrap } = useContract(
+  const { state: mintFirstOrbState, send: mintFirstOrb } = useContract(
     globals.AIKO,
     abi,
-    "deposit",
-    "Wrap"
+    "firstCyberOrbAiko",
+    "Mint first orb"
   );
-  return { wrapState, wrap };
+  return { mintFirstOrbState, mintFirstOrb };
+};
+
+export const useMintSecondOrb = () => {
+  const globals = useGlobals();
+
+  const { state: mintSecondtOrbState, send: mintSecondOrb } = useContract(
+    globals.AIKO,
+    abi,
+    "secondCyberOrbAiko",
+    "Mint second orb"
+  );
+  return { mintSecondtOrbState, mintSecondOrb };
+};
+
+export const useMintHolders = () => {
+  const globals = useGlobals();
+
+  const { state: mintHoldersState, send: mintHolders } = useContract(
+    globals.AIKO,
+    abi,
+    "holderAikoSale",
+    "Mint holders"
+  );
+  return { mintHoldersState, mintHolders };
 };
