b415dbb7a0b11ce58b35063e9c211395b4740e38
Ivan Martinez <ivanthegreatdev@gmail.com>
2022-06-24 19:57:01 -0400

    Change global

--- src/app/globals.ts
+++ src/app/globals.ts
@@ -1,15 +1,15 @@
 export const INFURA_ID = "ce27c65027c64f7a8ea17c6e63dfae96";
 export const OPENSEA = "https://opensea.io/";
-export const SALE_START = 1656169200000;
+export const SALE_START = 1656115000000;
 
 interface GlobalsType {
   AIKO: string;
 }
 
 export const ETH_GLOBALS: GlobalsType = {
-  AIKO: "0x93Df468Eb02fc4A68D838d75d23C77651a0FfDF4",
+  AIKO: "0xFd964900A30A5F24480c6f7c359bD58a735c0320",
 };
 
 export const RINKEBY_GLOBALS: GlobalsType = {
-  AIKO: "0x93Df468Eb02fc4A68D838d75d23C77651a0FfDF4",
+  AIKO: "0xFd964900A30A5F24480c6f7c359bD58a735c0320",
 };
