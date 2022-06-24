08a54c27cbead0a7aa7304886b218351c1b3f009
chase-manning <chase@manning.dev>
2022-06-24 22:23:54 +0100

    :wrench: update sale start time

--- src/app/globals.ts
+++ src/app/globals.ts
@@ -1,15 +1,15 @@
 export const INFURA_ID = "ce27c65027c64f7a8ea17c6e63dfae96";
 export const OPENSEA = "https://opensea.io/";
-export const SALE_START = 1656139991000;
+export const SALE_START = 1656106555000;
 
 interface GlobalsType {
   AIKO: string;
 }
 
 export const ETH_GLOBALS: GlobalsType = {
-  AIKO: "0x4b1145ED577CaCF5E93Ffd237a1487465fe06B61",
+  AIKO: "0x93Df468Eb02fc4A68D838d75d23C77651a0FfDF4",
 };
 
 export const RINKEBY_GLOBALS: GlobalsType = {
-  AIKO: "0x4b1145ED577CaCF5E93Ffd237a1487465fe06B61",
+  AIKO: "0x93Df468Eb02fc4A68D838d75d23C77651a0FfDF4",
 };
