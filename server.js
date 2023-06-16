a69414674761475d5c80ab49a180aac5174f8a73
Patrickrgt <patrickrgt@gmail.com>
2023-06-16 11:19:09 -0400

    Dependencies and setup for stamp redeem

---
+++ server.js
@@ -0,0 +1,132 @@
+const express = require("express");
+const fetch = require("node-fetch");
+const { request } = require("graphql-request");
+const dotenv = require("dotenv");
+dotenv.config();
+
+require("@shopify/shopify-api/adapters/node");
+// const { shopifyApi, LATEST_API_VERSION } = require("@shopify/shopify-api");
+// const cors = require("cors");
+const ShopifyApi = require("@shopify/shopify-api");
+
+const {
+  shopifyApi,
+  ApiVersion,
+  BillingInterval,
+} = require("@shopify/shopify-api");
+const { restResources } = require("@shopify/shopify-api/rest/admin/2022-07");
+// const Shopify = require("shopify-api-node");
+
+const app = express();
+app.use(express.json());
+const { Auth, Route } = ShopifyApi;
+
+const port = process.env.PORT || 3001;
+
+const shopifyStore = process.env.SHOPIFY_STORE;
+const apiKey = process.env.SHOPIFY_API_KEY;
+const apiAdminKey = process.env.SHOPIFY_API_PASSWORD;
+
+const apiEndpoint = `https://${apiKey}:${apiAdminKey}@${shopifyStore}.myshopify.com/admin/api/2022-04/graphql.json`;
+
+app.get("/get-discounts", async (req, res) => {
+  try {
+    const response = await fetch(
+      `https://${shopifyStore}.myshopify.com/admin/api/2023-04/price_rules.json`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          "X-Shopify-Access-Token": apiAdminKey,
+        },
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.post("/generate-discount", async (req, res) => {
+  try {
+    const { basicCodeDiscount } = req.body;
+
+    const discountCodeMutation = `
+      mutation discountCodeBasicCreate($basicCodeDiscount: DiscountCodeBasicInput!) {
+        discountCodeBasicCreate(basicCodeDiscount: $basicCodeDiscount) {
+          codeDiscountNode {
+            codeDiscount {
+              ... on DiscountCodeBasic {
+                title
+                codes(first: 10) {
+                  nodes {
+                    code
+                  }
+                }
+                startsAt
+                endsAt
+                customerSelection {
+                  ... on DiscountCustomerAll {
+                    allCustomers
+                  }
+                }
+                customerGets {
+                  value {
+                    ... on DiscountPercentage {
+                      percentage
+                    }
+                  }
+                  items {
+                    ... on AllDiscountItems {
+                      allItems
+                    }
+                  }
+                }
+                appliesOncePerCustomer
+              }
+            }
+          }
+          userErrors {
+            field
+            code
+            message
+          }
+        }
+      }
+    `;
+
+    const variables = {
+      basicCodeDiscount,
+    };
+
+    const discountCodeResponse = await request(
+      apiEndpoint,
+      discountCodeMutation,
+      variables
+    );
+    const { discountCodeBasicCreate } = discountCodeResponse;
+
+    if (discountCodeBasicCreate.userErrors.length > 0) {
+      return res
+        .status(400)
+        .json({ errors: discountCodeBasicCreate.userErrors });
+    }
+
+    res
+      .status(200)
+      .json({ discountCode: discountCodeBasicCreate.codeDiscountNode });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
--- src/components/userpanel/StampRedeem.tsx
+++ src/components/userpanel/StampRedeem.tsx
@@ -1,8 +1,35 @@
+// import dotenv from "dotenv";
+import React, { useState, useEffect } from "react";
+// import { BigNumber } from "ethers";
+import { useSelector } from "react-redux";
 import styled from "styled-components";
-import { useState } from "react";
+import * as CryptoJS from "crypto-js";
+import { selectGlobalAccount, selectStampsHeld } from "../../state/uiSlice";
 import ButtonBlue from "./ButtonBlue";
 import DecorHorizontal from "./DecorHorizontal";
-// import Countdown from "./Countdown";
+
+interface DecryptedObjectShape {
+  rewards: number;
+  wallet: number;
+}
+
+interface BasicCodeDiscount {
+  title: string;
+  code: string;
+  startsAt: string;
+  endsAt: string;
+  customerSelection: { all: boolean };
+  customerGets: {
+    value: { percentage: number };
+    items: {
+      products: {
+        productsToAdd: string[];
+      };
+    };
+  };
+  appliesOncePerCustomer: boolean;
+  usageLimit: number;
+}
 
 const RedeemContainer = styled.div`
   display: flex;
@@ -18,32 +45,259 @@ const ShippingText = styled.span`
   justify-content: center;
   align-items: center;
   display: flex;
-  font-size: 2vh;
+  font-size: 1.425vh;
   font-weight: 800;
   color: white;
   text-shadow: -2px 2px 0 #000, 2px 2px 0 #000, 2px -2px 0 #000,
     -2px -2px 0 #000;
 `;
 
+const ShippingPrice = styled.h1`
+  margin: 0;
+  font-size: 2.5rem;
+  font-weight: 800;
+  color: #ead182;
+  text-shadow: -2px 2px 0 #000, 2px 2px 0 #000, 2px -2px 0 #000,
+    -2px -2px 0 #000;
+`;
+
+// const DiscountCode = styled.div`
+//   position: relative;
+//   width: 100px;
+//   height: 100px;
+//   font-size: 18px;
+// `;
+
 const StampRedeem = () => {
-  const [disabled] = useState(true);
+  const walletAddress = useSelector(selectGlobalAccount);
+
+  // const ShopifyKey = process.env.SHOPIFY_API_PASSWORD;
+  const url = "http://localhost:3000/";
+
+  // const [title, setTitle] = useState("");
+  // const [code, setCode] = useState("");
+  const [result, setResult] = useState("");
+  const [shortWallet, setShortWallet] = useState("");
+  const [addProducts, setAddProducts] = useState([""]);
+  const [disabled, setDisabled] = useState(false);
+
+  const [encryptedObject, setEncryptedObject] = useState("");
+  const [decryptedObject, setDecryptedObject] = useState<DecryptedObjectShape>({
+    rewards: 0,
+    wallet: 0,
+  });
+
+  const [basicCodeDiscount, setBasicCodeDiscount] = useState<BasicCodeDiscount>(
+    {
+      title: "",
+      code: "",
+      startsAt: "",
+      endsAt: "",
+      customerSelection: { all: false },
+      customerGets: {
+        value: { percentage: 0 },
+        items: {
+          products: {
+            productsToAdd: [""],
+          },
+        },
+      },
+      appliesOncePerCustomer: false,
+      usageLimit: 0,
+    }
+  );
+
+  const [key] = useState("aikoaikoaiko");
+
+  const stampsHeld = useSelector(selectStampsHeld);
+
+  function toBase64Url(base64: string): string {
+    return base64.replace("+", "-").replace("/", "_").replace(/=+$/, "");
+  }
+
+  function fromBase64Url(base64url: string): string {
+    base64url = base64url.replace("-", "+").replace("_", "/");
+    while (base64url.length % 4) {
+      base64url += "=";
+    }
+    return base64url;
+  }
+
+  function encrypt(object: object, key: string): string {
+    const objectString: string = JSON.stringify(object);
+    const ciphertext = CryptoJS.AES.encrypt(objectString, key);
+    return toBase64Url(ciphertext.toString());
+  }
+
+  function decrypt(ciphertext: string, key: string): DecryptedObjectShape {
+    const bytes = CryptoJS.AES.decrypt(fromBase64Url(ciphertext), key);
+    const decryptedData: string = bytes.toString(CryptoJS.enc.Utf8);
+    return JSON.parse(decryptedData);
+  }
+
+  function isValidDecryptedObject(object: any): object is DecryptedObjectShape {
+    return (
+      object &&
+      typeof object === "object" &&
+      "rewards" in object &&
+      "wallet" in object
+    );
+  }
+
+  useEffect(() => {
+    if (walletAddress) {
+      const firstFive = walletAddress.slice(0, 5);
+      const lastFive = walletAddress.slice(-5);
+      setShortWallet(`${firstFive}...${lastFive}`);
+    }
+  }, [walletAddress]);
+
+  useEffect(() => {
+    if (encryptedObject) {
+      console.log("Encrypted object:", encryptedObject);
+      window.open(`${url}?coupon=${encryptedObject}`, "_blank");
+
+      // Set basicCodeDiscount with the decrypted object
+      setBasicCodeDiscount({
+        title: `${walletAddress}`,
+        code: `${encryptedObject}`,
+        startsAt: "2022-06-21T00:00:00Z",
+        endsAt: "2023-09-21T00:00:00Z",
+        customerSelection: { all: true },
+        customerGets: {
+          value: { percentage: 1 },
+          items: {
+            products: {
+              productsToAdd: addProducts,
+            },
+          },
+        },
+        appliesOncePerCustomer: false,
+        usageLimit: 1,
+      });
+      const object = decrypt(encryptedObject, key);
+      if (isValidDecryptedObject(object)) {
+        setDecryptedObject(object);
+        console.log("Decrypted object:", object);
+      } else {
+        console.error(
+          "Decrypted object does not have the expected shape:",
+          object
+        );
+      }
+    }
+  }, [encryptedObject, key]);
+
+  useEffect(() => {
+    const postDiscount = async () => {
+      try {
+        fetch("/get-discounts")
+          .then((response) => {
+            if (!response.ok) {
+              throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            return response.json();
+          })
+          .then(async (data) => {
+            // Do something with 'data', which contains the response from the Express endpoint
+            const discounts = data.price_rules;
+            const exists = discounts.find(
+              (discount: any) => discount.title === walletAddress
+            );
+
+            if (exists) {
+              console.log(
+                "Discount exists. Will not generate new discount:",
+                exists
+              );
+            } else {
+              console.log(
+                "No discount found with the title. Generating new discount:",
+                walletAddress
+              );
+              const response = await fetch("/generate-discount", {
+                method: "POST",
+                headers: {
+                  "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ basicCodeDiscount }),
+              });
+
+              const data = await response.json();
+
+              if (response.ok) {
+                setResult(
+                  `Discount code created: ${data.discountCode.codeDiscount.codes.nodes[0].code}`
+                );
+              } else {
+                setResult(`Error: ${data.errors[0].message}`);
+              }
+            }
+          })
+          .catch((error) => {
+            console.error("Error:", error);
+          });
+      } catch (error) {
+        setResult(`Error: ${(error as Error).message}`);
+      }
+    };
+    console.log(basicCodeDiscount);
+    postDiscount();
+  }, [basicCodeDiscount]);
+
+  const handleButtonClick = async () => {
+    // const walletCode = BigNumber.from(walletAddress);
+    // const decimalString = walletCode.toString();
+    // const number = Number(decimalString);
+
+    if (stampsHeld >= 9) {
+      const object = {
+        rewards: 3,
+        wallet: shortWallet,
+      };
+      await setAddProducts([
+        "gid://shopify/Product/8312585650467",
+        "gid://shopify/Product/8312587583779",
+        "gid://shopify/Product/8312588566819",
+      ]);
+      await setEncryptedObject(encrypt(object, key));
+    } else if (stampsHeld >= 6) {
+      const object = {
+        rewards: 2,
+        wallet: shortWallet,
+      };
+      await setAddProducts([
+        "gid://shopify/Product/8312585650467",
+        "gid://shopify/Product/8312587583779",
+      ]);
+      await setEncryptedObject(encrypt(object, key));
+    } else if (stampsHeld >= 3) {
+      const object = {
+        rewards: 1,
+        wallet: shortWallet,
+      };
+      await setAddProducts(["gid://shopify/Product/8312585650467"]);
+      await setEncryptedObject(encrypt(object, key));
+    } else {
+      setDisabled(true);
+    }
+  };
 
   return (
     <RedeemContainer>
       <ShippingText>
-        EARLY JULY
-        {/* Countdown to StampRedeem Functionality, used as placeholder */}
-        {/* <Countdown /> */}
+        SHIPPING FEE: &nbsp; <ShippingPrice>ETA</ShippingPrice>
       </ShippingText>
       <ButtonBlue
         disabled={disabled}
         close={() => {
-          return;
+          handleButtonClick();
         }}
         content="REDEEM"
       />
 
       <DecorHorizontal height={3} />
+      {/* <DiscountCode>{result}</DiscountCode> */}
     </RedeemContainer>
   );
 };
